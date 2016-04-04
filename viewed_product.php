742c6148a93aa89df08453d6a247adfd4b6a48a6
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2016-04-04 13:11:38 +0300

    viewed_product
    viewed_product

---
+++ viewed_product.php
@@ -0,0 +1,26 @@
+<?
+$countViewedProducts = 0;
+		$GLOBALS['arViewedProducts'] = array();
+		if(\Bitrix\Main\Loader::includeModule("catalog") && \Bitrix\Main\Loader::includeModule("sale"))
+		{
+		   $arFilter["FUSER_ID"] = CSaleBasket::GetBasketUserID();
+		   if(\Bitrix\Main\Config\Option::get("sale", "viewed_capability", "") == "Y")
+		   {
+		      $viewedIterator = \Bitrix\Catalog\CatalogViewedProductTable::getList(
+		         array(
+		            "filter" => $arFilter,
+		            "select" => array(
+		               "ID", "PRODUCT_ID"
+		            ),
+		            "order" => array("DATE_VISIT" => "DESC"),
+		         )
+		      );
+		
+		      while($row = $viewedIterator->fetch())
+		      {
+		         $GLOBALS['arViewedProducts'][] = $row['PRODUCT_ID'];
+		         $countViewedProducts++;
+		      }
+		   }
+		}
+?>
