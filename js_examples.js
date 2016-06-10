b0eb35d7b26dd037edd9349b69287c304be30dca
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2016-06-10 17:00:42 +0300

    //обновления параметров URL
    //обновления параметров URL

--- js_examples.js
+++ js_examples.js
@@ -37,3 +37,33 @@ $("select option").val(function(i,v){
   $(this).siblings("[value="+ v.replace(/\s{1,}/g, '_') +"]").remove();
 });
 
+//обновления параметров URL
+function UpdateQueryString(key, value, url) {
+    if (!url) url = window.location.href;
+    var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),
+        hash;
+
+    if (re.test(url)) {
+        if (typeof value !== 'undefined' && value !== null)
+            return url.replace(re, '$1' + key + "=" + value + '$2$3');
+        else {
+            hash = url.split('#');
+            url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+                url += '#' + hash[1];
+            return url;
+        }
+    }
+    else {
+        if (typeof value !== 'undefined' && value !== null) {
+            var separator = url.indexOf('?') !== -1 ? '&' : '?';
+            hash = url.split('#');
+            url = hash[0] + separator + key + '=' + value;
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+                url += '#' + hash[1];
+            return url;
+        }
+        else
+            return url;
+    }
+}
