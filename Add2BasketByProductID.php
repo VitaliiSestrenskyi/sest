9a5cd8b4d79f889e0a9c76e6e97e56a3c28499f6
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2016-09-27 00:48:06 +0300

    реализовал через ф-цию
    если товары не добавляются в корзину смотрим - $APPLICATION->GetException();

--- Add2BasketByProductID.php
+++ Add2BasketByProductID.php
@@ -1,15 +1,49 @@
-<?php
-$result =  Add2BasketByProductID(
-				 6493, // ID товара
-				 1, //количество
-				 array( 'DELAY'=>'Y' ),	 // список полей с https://dev.1c-bitrix.ru/api_help/sale/classes/csalebasket/csalebasket__add.php
-				 array( 
-				 		 array(  'NAME' => 'Наличие',
-					         	'SORT' => 20,
-					         	'CODE' => 'STATUS_AVAIL',
-					         	'VALUE' => false
-				          	)				         
-					  )
-					  ); 
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+CModule::IncludeModule("catalog");
+CModule::IncludeModule("sale");
 
-?>
+$pid    	= (int)$_REQUEST['idProduct'];
+$quantity   = (!empty($_REQUEST['quantity'])) ? $_REQUEST['quantity'] : 1 ;
+
+
+function AddToBasket( $pid,$quantity )
+{
+    if (CModule::IncludeModule("sale") && CModule::IncludeModule("catalog") )
+    {
+        $ar_res = CCatalogProduct::GetByIDEx($pid);
+        if($ar_res){
+            $result =  Add2BasketByProductID($pid,$quantity); // если ошибки -  $ex = $APPLICATION->GetException();
+            if($result){
+                $arrSendResult['ERROR'] = 'N';
+                $arrSendResult['TEXT'] = 'OK';
+                echo json_encode($arrSendResult);
+            }else{
+                $arrSendResult['ERROR'] = 'Y';
+                $arrSendResult['TEXT'] = 'Error';
+                echo json_encode($arrSendResult);
+            }
+        }else{
+            if( Add2BasketByProductID($pid,$quantity) )
+            {
+                $arrSendResult['ERROR'] = 'N';
+                $arrSendResult['TEXT'] = 'OK';
+                echo json_encode($arrSendResult);
+            }
+        }
+    }
+}
+
+
+
+if( !empty($pid)  && !empty($quantity) )
+{
+    AddToBasket( $pid,$quantity );
+}
+else
+{
+    $arrSendResult['ERROR'] = 'Y';
+    $arrSendResult['TEXT'] = 'Error with parameters';
+    echo json_encode($arrSendResult);
+}
+
+
