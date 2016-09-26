7a20aa8905746ae6b0bc652053552e90ef1c2a66
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2016-09-26 23:38:28 +0300

    Update cache.php

--- cache.php
+++ cache.php
@@ -19,6 +19,69 @@
 		$obCache->EndDataCache(array('FEEDBACKS' => $arResult ));
 	}
 
+////////////////////////////////////////////////////////////////////////////////////////////
+// тегированый кеш 
+//http://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=43&LESSON_ID=2978&LESSON_PATH=3913.4565.4780.2978
+if (!isset($arParams['FILTER_VIEW_MODE']) || (string)$arParams['FILTER_VIEW_MODE'] == '')
+	$arParams['FILTER_VIEW_MODE'] = 'VERTICAL';
+$arParams['USE_FILTER'] = (isset($arParams['USE_FILTER']) && $arParams['USE_FILTER'] == 'Y' ? 'Y' : 'N');
+
+$isVerticalFilter = ('Y' == $arParams['USE_FILTER'] && $arParams["FILTER_VIEW_MODE"] == "VERTICAL");
+$isSidebar = ($arParams["SIDEBAR_SECTION_SHOW"] == "Y" && isset($arParams["SIDEBAR_PATH"]) && !empty($arParams["SIDEBAR_PATH"]));
+$isFilter = ($arParams['USE_FILTER'] == 'Y');
+
+if ($isFilter)
+{
+	$arFilter = array(
+		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
+		"ACTIVE" => "Y",
+		"GLOBAL_ACTIVE" => "Y",
+	);
+	if (0 < intval($arResult["VARIABLES"]["SECTION_ID"]))
+		$arFilter["ID"] = $arResult["VARIABLES"]["SECTION_ID"];
+	elseif ('' != $arResult["VARIABLES"]["SECTION_CODE"])
+		$arFilter["=CODE"] = $arResult["VARIABLES"]["SECTION_CODE"];
+
+	$obCache = new CPHPCache();
+	if ($obCache->InitCache(36000, serialize($arFilter), "/iblock/catalog"))
+	{
+		$arCurSection = $obCache->GetVars();
+	}
+	elseif ($obCache->StartDataCache())
+	{
+		$arCurSection = array();
+		if (Loader::includeModule("iblock"))
+		{
+			$dbRes = CIBlockSection::GetList(array(), $arFilter, false, array("ID"));
+
+			if(defined("BX_COMP_MANAGED_CACHE"))
+			{
+				global $CACHE_MANAGER;
+				$CACHE_MANAGER->StartTagCache("/iblock/catalog");
+
+				if ($arCurSection = $dbRes->Fetch())
+					$CACHE_MANAGER->RegisterTag("iblock_id_".$arParams["IBLOCK_ID"]);
+
+				$CACHE_MANAGER->EndTagCache();
+			}
+			else
+			{
+				if(!$arCurSection = $dbRes->Fetch())
+					$arCurSection = array();
+			}
+		}
+		$obCache->EndDataCache($arCurSection);
+	}
+	if (!isset($arCurSection))
+		$arCurSection = array();
+}
+
+
+
+
+
+
+
 
 
 
