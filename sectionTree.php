1167886543f249642d9355120f97b89d056ffc40
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2016-09-28 12:00:31 +0300

    получаем все подразделы текущего раздела
    получаем все подразделы текущего раздела

--- sectionTree.php
+++ sectionTree.php
@@ -15,4 +15,19 @@ $arFilter = array(
 			    $sectionLinc[intval($arSection['IBLOCK_SECTION_ID'])]['CHILD'][$arSection['ID']] = $arSection;
 			    $sectionLinc[$arSection['ID']] = &$sectionLinc[intval($arSection['IBLOCK_SECTION_ID'])]['CHILD'][$arSection['ID']];
 			}
+
+//или так получаем все подразделы текущего раздела
+
+$sections = GetIBlockSectionList($arParams['IBLOCK_ID_SECTIONS'],
+            false,
+            Array(),
+            false,
+            Array("LEFT_MARGIN"=>$category['LEFT_MARGIN'], "RIGHT_MARGIN"=>$category['RIGHT_MARGIN'])
+        );
+        $ids = [];
+        while( $resSub = $sections->fetch())
+        {
+            $ids[]  = $resSub['ID'];
+        }
+
 ?>
