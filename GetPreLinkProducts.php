dad471efa9710ad7e139f36f0a08033924a09de2
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2016-10-10 11:20:59 +0300

    добавил 2 вариант
    добавил 2 вариант

--- GetPreLinkProducts.php
+++ GetPreLinkProducts.php
@@ -23,4 +23,66 @@ function GetPreLinkProducts( $iblockId, $elementId, $showCountElBySides )
 			return $linkProds;	
 	}
 }
+
+
+ function GetPreLinkProducts2( $IBLOCK_ID_CATALOG, $SECTION_ID, $nElementID, $nPageSize )
+    {
+        $res = CIBlockElement::GetList(
+            array(
+                'sort' => 'asc'
+            ),
+            array(
+                'IBLOCK_ID' => $IBLOCK_ID_CATALOG,
+                'ACTIVE' => 'Y',
+                'SECTION_ID' =>$SECTION_ID
+            ),
+            false,
+            array(
+                'nElementID' => $nElementID,
+                'nPageSize' => $nPageSize
+            )
+        );
+
+        $nearElements = [];
+        while ($arElem = $res->GetNext())
+        {
+            if ($arElem['ID'] != $nElementID)
+                $nearElements[] = (int)$arElem['ID'];
+        }
+
+        if(count($nearElements)==4)
+        {
+            return $nearElements;
+        }
+        else
+        {
+            $res = CIBlockElement::GetList(
+                array(
+                    'sort' => 'asc'
+                ),
+                array(
+                    'IBLOCK_ID' => $IBLOCK_ID_CATALOG,
+                    'ACTIVE' => 'Y',
+                    'SECTION_ID' =>$SECTION_ID
+                ),
+                false,
+                array(
+                    'nElementID' => $nElementID,
+                    'nPageSize' => 10
+                )
+            );
+
+            $nearElements2 = [];
+            while ($arElem = $res->GetNext())
+            {
+                if ($arElem['ID'] != $nElementID)
+                    $nearElements2[] = (int)$arElem['ID'];
+            }
+            return $nearElements2;
+        }
+    }
+
+
+
+
 ?>
